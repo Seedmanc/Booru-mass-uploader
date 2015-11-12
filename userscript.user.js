f5fff1d7bd03108374dbaf38f3b98d91a2d8e805
Dmitry <seedmanc@yahoo.com>
2015-11-13 02:19:24 +0300

    load html w/o jq

--- userscript.user.js
+++ userscript.user.js
@@ -8,7 +8,7 @@
 function loadAndExecute(url, callback){											
 	var scriptNode = document.createElement ("script");	
 	scriptNode.addEventListener("load", callback);
-	scriptNode.onerror=function(){ 
+	scriptNode.onerror = function(){ 
 		throw new Error("Can't load "+url);
 	};
 	scriptNode.src = url;
@@ -18,7 +18,17 @@ function loadAndExecute(url, callback){
 document.addEventListener('DOMContentLoaded', function(){
 	if (window.top != window.self)  											
 		return;
-	loadAndExecute("https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", main);
+	//loadAndExecute("https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", main);
+	
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', 'http://crossorigin.me/http://seedmanc.github.io/Booru-mass-uploader/index.html', true);
+	xhr.onreadystatechange= function() {
+		if (this.readyState != 4) return;
+		if (this.status != 200) return; // or whatever error handling you want
+		document.getElementsByTagName('body')[0].innerHTML = this.responseText;
+	};
+	xhr.send();
+	
 }, false);
 
 function main(){
