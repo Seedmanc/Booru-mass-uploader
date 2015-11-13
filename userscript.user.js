f1716643741c2582ac3f5973db16895816f2d410
Dmitry <seedmanc@yahoo.com>
2015-11-13 15:59:23 +0300

    error handlin'

--- userscript.user.js
+++ userscript.user.js
@@ -3,7 +3,7 @@
 // @description	 Add ability to bulk upload images to your booru
 // @version	1.0
 // @author		Seedmanc
-// @include	http://*.booru.org/*
+// @include	http://*.booru.org/index.php*
 // ==/UserScript==
 
 function loadAndExecute(url, callback){											
@@ -25,29 +25,30 @@ function loadAndExecute(url, callback){
 		xhr.open('GET', 'http://crossorigin.me/http://seedmanc.github.io/Booru-mass-uploader/index.html', true);
 		xhr.onreadystatechange= function() {
 			if (this.readyState != 4) return;
-			if (this.status != 200) return; // or whatever error handling you want
+			if (this.status != 200) return; 
 			document.getElementsByTagName('body')[0].innerHTML = this.responseText;
 			var s = document.getElementsByTagName('script');
 			for (var i = 0; i < s.length ; i++) {
-				var node=s[i], parent=node.parentElement, d = document.createElement('script');
-				d.async=node.async;
-				d.src=node.src;
-				parent.insertBefore(d,node);
+				var node=s[i], parent = node.parentElement, d = document.createElement('script');
+				d.async = node.async;
+				d.src = node.src;
+				parent.insertBefore(d, node);
 				parent.removeChild(node);
 			}
 		};
 		xhr.send();
-	} else if (~document.location.href.indexOf('?page='))
-		//document.addEventListener('DOMContentLoaded', function()
+	} else if (~document.location.href.indexOf('?page=')) 
 		{
-			var navbar = document.getElementById('navbar');
+		  try {
 			var notice = document.getElementById('notice').previous() ;
 			notice.parentNode.removeChild(notice);
 			var longnotice = document.getElementById('long-notice');
 			longnotice.parentNode.removeChild(longnotice);
 			var ad = document.querySelectorAll('center div[id*="adbox"]')[0].parentNode;
 			ad.parentNode.removeChild(ad);
+		  } catch(any){};
 			
+			var navbar = document.getElementById('navbar');			
 			var li = document.createElement("li");
 			var a  = document.createElement("a");
 			a.style= "font-weight:bold;";
@@ -55,5 +56,5 @@ function loadAndExecute(url, callback){
 			a.href = document.location.href.split('.')[0]+'.booru.org/index.php?page=post&s=mass_upload';
 			li.appendChild(a);
 			navbar.appendChild(li);
-		}//, false); 	
+		} 	
 })(); 
