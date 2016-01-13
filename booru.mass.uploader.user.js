43347a481092cba777d1b8f45ff815a0d301dae4
Dmitry <seedmanc@yahoo.com>
2016-01-13 21:26:06 +0300

    Fix bold for Chrome
    as always Opera has no problems

--- booru.mass.uploader.user.js
+++ booru.mass.uploader.user.js
@@ -53,7 +53,7 @@ if (~document.location.href.indexOf('s=mass_upload')) {
 		}
 	} catch (any) {}
 
-	a.style = "font-weight:bold;";
+	a.style.fontWeight = 'bold';
 	a.appendChild(document.createTextNode('Mass Upload'));
 	a.href = document.location.protocol + '//' + document.location.hostname + '/index.php?page=post&s=mass_upload';
 	li.appendChild(a);
