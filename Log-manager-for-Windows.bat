d2cae89986728224d2b8e3feb36bb1177f4f3580
ProximaNova <jaretrudd@gmail.com>
2016-03-14 12:11:48 -0600

    Create Log-manager-for-Windows.bat

---
+++ Log-manager-for-Windows.bat
@@ -0,0 +1,16 @@
+:: Uuse the following command in Vim on the log:
+:: %s/^.* Image \(.* was successfully uploaded\.$\)\@=/del "/ge | %s/.* Couldn't upload \(.*: image already exists here\.\)\@=/del "/ge | %s/: image already exists here\.$/"/ge | %s/ was successfully uploaded\.$/"/ge | %s/.* Couldn't upload \(.*: image too big? too small? corrupted?\.\)\@=/move "/ge | %s/: image too big? too small? corrupted?\./" upload_failure_dimensions/ge | %s/.* Couldn't upload \(.*: undefined\.$\)\@=/move "/ge | %s/: undefined\.$/" upload_failure_undefined/ge | g/^$/d
+
+If Not Exist "upload_failure_dimensions\" (
+    mkdir upload_failure_dimensions
+)
+
+If Not Exist "upload_failure_undefined\" (
+    mkdir upload_failure_undefined
+)
+
+TIMEOUT 3
+
+:: [Replace this line with the edited (see above) log text without the "Started..." and "Finished..." part at the top and bottom.]
+
+TIMEOUT 999
