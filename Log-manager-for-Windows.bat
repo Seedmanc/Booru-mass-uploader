d6e28eef32c85872c336a991e2b499fab26b0bf3
ProximaNova <jaretrudd@gmail.com>
2016-03-15 01:33:28 -0600

    Update Log-manager-for-Windows.bat

--- Log-manager-for-Windows.bat
+++ Log-manager-for-Windows.bat
@@ -1,5 +1,5 @@
-:: Uuse the following command in Vim on the log:
-:: %s/^.* Image \(.* was successfully uploaded\.$\)\@=/del "/ge | %s/.* Couldn't upload \(.*: image already exists here\.\)\@=/del "/ge | %s/: image already exists here\.$/"/ge | %s/ was successfully uploaded\.$/"/ge | %s/.* Couldn't upload \(.*: image too big? too small? corrupted?\.\)\@=/move "/ge | %s/: image too big? too small? corrupted?\./" upload_failure_dimensions/ge | %s/.* Couldn't upload \(.*: undefined\.$\)\@=/move "/ge | %s/: undefined\.$/" upload_failure_undefined/ge | g/^$/d
+:: Use the following command in Vim on the log:
+:: %s/^.* Started uploading.*\.$//ge | %s/^.* Finished uploading.*total\.$//ge | %s/^.* Image \(.* was successfully uploaded\.$\)\@=/del "/ge | %s/.* Couldn't upload \(.*: image already exists here\.\)\@=/del "/ge | %s/: image already exists here\.$/"/ge | %s/ was successfully uploaded\.$/"/ge | %s/.* Couldn't upload \(.*: image too big? too small? corrupted?\.\)\@=/move "/ge | %s/: image too big? too small? corrupted?\./" upload_failure_dimensions/ge | %s/.* Couldn't upload \(.*: undefined\.$\)\@=/move "/ge | %s/: undefined\.$/" upload_failure_undefined/ge | g/^$/d
 
 If Not Exist "upload_failure_dimensions\" (
     mkdir upload_failure_dimensions
@@ -11,6 +11,6 @@ If Not Exist "upload_failure_undefined\" (
 
 TIMEOUT 3
 
-:: [Replace this line with the edited (see above) log text without the "Started..." and "Finished..." part at the top and bottom.]
+:: [Replace this line with the log text.]
 
 TIMEOUT 999
