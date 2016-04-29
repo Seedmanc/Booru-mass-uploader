7282f127cc4c6236a98679c0e23ee5113dde6b26
Dmitry <seedmanc@yahoo.com>
2016-04-29 20:52:34 +0300

    NO MORE CORS
    Get rid of this crap entirely. No more relying on the unstable proxies with no uptime, no more FF problems with script loading.
    I should have thought of simply putting the entire html page into a .js file earlier. Where are your security limitations now?
    Since jQuery isn't needed anymore either, the support for Opera is back as well.
    
    It's a real pity I can't use the same approach in my MSL project.

--- booru.mass.uploader.user.js
+++ booru.mass.uploader.user.js
@@ -2,7 +2,7 @@
 // @name		Booru Mass Uploader
 // @description	Add ability to bulk upload images to your booru
 // @namespace 	https://github.com/Seedmanc/Booru-mass-uploader
-// @version     1.3.2
+// @version     1.3.3
 // @author		Seedmanc
 // @include     http://*.booru.org/index.php*
 // @include     http://rule34.xxx/index.php*
@@ -13,82 +13,64 @@
 // @include		https://chan.sankakucomplex.com/*
 // @include		http://atfbooru.ninja/*
 // @include		http://danbooru.donmai.us/*
+// @include		http://eikonos.org/*
 
 // you can add any boorus of your choice by following the pattern
 
 // @grant 		none
 // @run-at		document-end
 // @noframes
-
-// @require  	https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // ==/UserScript==
 
 if (window.top != window.self) {
 	throw 'no iframes';
 }
 
-var corses = ['www.whateverorigin.org/get?url=', 'crossorigin.me/'];
+function activateScripts(scripts, i) {
+	var node   = scripts[i],
+	    parent = node.parentElement,
+	    d      = document.createElement('script');
 
-function onSuccess(response) {
-	var scripts;
-
-	document.getElementsByTagName('body')[0].innerHTML = response;
-	scripts = document.getElementsByTagName('script');
-
-	for (var i = 0; i < scripts.length; i++) {
-		var node = scripts[i], parent = node.parentElement, d = document.createElement('script');
-
-		d.async = node.async;
-		d.src = node.src;
-		parent.insertBefore(d, node);
-		parent.removeChild(node);
-	}
-}
-
-function tryCors(idx) {
-
-	idx = idx || 0;
-
-	if (!corses[idx]) {
-		document.write('Ran out of CORS engines, try later');
-		return;
-	}
-
-	(idx ? $.get : $.getJSON)(location.protocol + '//' + corses[idx] + 'http://seedmanc.github.io/Booru-mass-uploader/index.html' + (idx ? '' : '&callback=?'))
-		.done(function (response) {
-			onSuccess(response.contents || response);
-		})
-		.fail(function (response) {
-			if (response.status == 200 && response.readyState == 4) {
-				onSuccess(response.responseText);
-			} else {
-				console.log(response);
-				tryCors(idx + 1);
-			}
-		});
+	d.async = node.async;
+	d.src = node.src;
+	d.onload = function () {
+		if (i < scripts.length - 1) {
+			activateScripts(scripts, i + 1);
+		}
+	};
+	parent.insertBefore(d, node);
+	parent.removeChild(node);
 }
 
 if (~document.location.href.indexOf('s=mass_upload')) {
-	document.write('<img src="https://seedmanc.github.io/Booru-mass-uploader/spinner.gif"/>');
-	tryCors();
+	var script = document.createElement('script');
+
+	document.body.innerHTML = '<img src="https://seedmanc.github.io/Booru-mass-uploader/spinner.gif"/>';
+	script.src = 'https://seedmanc.github.io/Booru-mass-uploader/js/index.html.js';
+	script.onload = function () {
+		var scripts = document.getElementsByTagName('script');
+
+		activateScripts(scripts, 0);
+	};
+	document.body.appendChild(script);
 
 } else {
-	var navbar = $('#navbar')[0] ||
-		$('.flat-list2')[0] ||
-		$('#main-menu > ul')[0] ||
-		$('nav > menu')[0];
+	var navbar = document.getElementById('navbar') ||
+		document.getElementsByClassName('flat-list2')[0] ||
+		document.querySelector('#main-menu > ul') ||
+		document.querySelector('nav > menu');
 	var li = document.createElement("li");
 	var a = document.createElement("a");
-	var token = $('meta[name="csrf-token"]')[0];
+	var token = document.querySelector('meta[name="csrf-token"]');
 
 	token = token && token.content;
 	if (token) {
 		localStorage.setItem('auth_token', token);
 	}
 
-	if ($('[src*="moe-legacy"]').length || $('html.action-post').length || $('[href*="/post/upload"]').length) {
+	if (document.querySelector('[src*="moe-legacy"]') || document.querySelector('html.action-post') || document.querySelector('[href*="/post/upload"]')) {
 		localStorage.setItem('current', 'moebooru');
-	} else if ($('[href*="/uploads/new"]').length || ~document.documentElement.innerHTML.indexOf('Running Danbooru')) {
+	} else if (document.querySelector('[href*="/uploads/new"]') || ~document.documentElement.innerHTML.indexOf('Running Danbooru')) {
 		localStorage.setItem('current', 'danbooru');
 	}
 
