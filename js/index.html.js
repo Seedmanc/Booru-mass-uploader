63632d5e01a9bb3bcb1d57be343f35f16e6b73cb
Dmitry <Seedmanc@users.noreply.github.com>
2018-09-07 11:10:38 +0300

    Update index.html.js

--- js/index.html.js
+++ js/index.html.js
@@ -129,7 +129,7 @@ document.documentElement.innerHTML='\
 	<script type="text/javascript" src="https://seedmanc.github.io/Booru-mass-uploader/js/uploader.js?v=1.4.1"></script>\
 </head>\
 <body>\
-<h1>Booru Mass-Uploader<span style="font-size:14px;"> v1.4.0</span></h1>\
+<h1>Booru Mass-Uploader<span style="font-size:14px;"> v1.4.2</span></h1>\
 <p class="intro">This script allows you to mass-upload images to imageboard sites running *booru software.\
 	Select a number of files and they\'re sent to the website along with the metadata you include such as\
 	the tags retrived from the filenames.</p>\
